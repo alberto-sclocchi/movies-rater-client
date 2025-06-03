13939002cd59f61458d436f4e829fcae9604a64a
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-03 00:09:13 -0400

    add style to searched movies with poster

--- src/components/movies/MoviesDashboard.jsx
+++ src/components/movies/MoviesDashboard.jsx
@@ -9,8 +9,9 @@ export default function MoviesDashboard() {
 
   const renderSearchedMovies = () => {
     return searchedMovies.map((movie, key) => (
-      <div key={key}>
+      <div key={key} className='searched-movie'>
         <h2>{movie.title} {movie.id}</h2>
+        <img src={movie.imageSet.verticalPoster.w720} alt={`${movie.title} poster`} />
       </div>    
     ));
   }
@@ -18,7 +19,7 @@ export default function MoviesDashboard() {
     <div className='movies-dashboard'>
         <h1>Movies Dashboard</h1>
         <SearchMoviesBar />
-        <div>
+        <div className='searched-movies'>
             {!!searchedMovies && searchedMovies.length ? renderSearchedMovies() : <h3>NO MOVIES</h3>}
         </div>
     </div>
