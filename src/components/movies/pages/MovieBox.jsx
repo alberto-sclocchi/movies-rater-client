0601e5e418681d2c4bc2120639f2947246bdbfe8
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-04 00:58:34 -0400

    show 'Already Added' message in search movies if movie was already added

--- src/components/movies/pages/MovieBox.jsx
+++ src/components/movies/pages/MovieBox.jsx
@@ -5,14 +5,24 @@ import { RenderType } from '../models/RenderType.model.js';
 export default function SearchedMovieBox(props) {
   
   const {id, title, releaseYear, overview, cast, renderType} = props.movie;
+  const { addedMovies } = useContext(MoviesContext);
 
   //  console.log({...props.movie});
-
+  const isAlreadyAdded = addedMovies.some((movie) => movie.movieId === id);
   const { addMovie } = useContext(MoviesContext);
 
   return (
      <div className='searched-movie'>
-        {renderType === RenderType.searchedMovie ? <button onClick={() => addMovie({movieId: id, title, releaseYear, verticalPoster: props.movie.imageSet.verticalPoster.w720, overview, cast})}>+</button> : <button>-</button>}
+        {
+          isAlreadyAdded
+          ? <p>Already Added</p>
+          : renderType === RenderType.searchedMovie 
+          ? <button onClick={() => addMovie({movieId: id, title, releaseYear, verticalPoster: props.movie.imageSet.verticalPoster.w720, overview, cast})}>+</button> 
+          : renderType === RenderType.addedMovie 
+          ? <button>-</button>
+          : <p>?</p>
+        }
+
         <h2>{title} ({releaseYear})</h2>
         {renderType === RenderType.searchedMovie ? <img src={props.movie.imageSet.verticalPoster.w720} alt={`${title} poster`} /> : <img src={props.movie.verticalPoster} alt={`${title} poster`} />}
     </div>
