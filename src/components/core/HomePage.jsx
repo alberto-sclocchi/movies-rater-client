46a888eadb8f357f13a36259242275003259e878
Alberto Sclocchi <scloks75tv@gmail.com>
2025-06-22 19:27:14 -0400

    create temporary home page

--- src/components/core/HomePage.jsx
+++ src/components/core/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import AuthContext from '../auth/context/AuthContext.context'
+import { Link } from 'react-router-dom';
+import popCornGif from '../../images/Popcorn.gif'
 
 export default function HomePage() {
   const { user, errorMessage, setErrorMessage} = useContext(AuthContext);
@@ -9,9 +11,23 @@ export default function HomePage() {
   }, [])
   
   return (
-    <div>
-      <h3>HomePage</h3>
+    <div id="home-page">
       {!!errorMessage && errorMessage}
+      <div className='home-page-div primary-home-page-div'>
+        <p>Discover new movies, rate your favorites, and get personalized recommendations from ReelBot.</p>
+        {!user ? <Link to="/signup" id="signup-home-page">Start exploring now!</Link> : <Link to="/dashboard" id="signup-home-page">Explore movies!</Link>}
+      </div>
+      <div className="home-page-div secondary-home-page-div">
+        <h2 style={{textDecoration: "underline"}}>Features</h2>
+        <ul>
+          <li>Add and rate your favorite movies 🎬 </li>
+          <li>Get smart suggestions from ReelBot 🤖</li>
+          <li>Track your movie journey ⭐</li>
+        </ul>
+      </div>
+      <div className='popcorn-div'>
+        <img src={popCornGif} alt="popcorn-gif" />
+      </div>
     </div>
   )
 }
